eb60eaaecdc46da07c2cd3eb8f33914adff5c7fc
eqfwcev123 <dohyeonee95@hotmail.com>
2020-01-17 15:29:03 +0900

    vote-list, vote-detail, url 연결

--- app/vote/urls.py
+++ app/vote/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from vote.views import vote_list_view, vote_detail_view
+from vote.views import vote_list_view, vote_detail_view, stat
 
 app_name = 'vote'
 urlpatterns = [
     path('list/', vote_list_view, name='vote-list'),
-    path('detail/<int:pk>/', vote_detail_view, name="vote-detail")
+    path('detail/<int:pk>/', vote_detail_view, name="vote-detail"),
+    path('stat/', stat, name='stat')
 ]
