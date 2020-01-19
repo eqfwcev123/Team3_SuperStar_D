6c57f1a1d164a57ea03b456ec34906e9806ad23b
eqfwcev123 <dohyeonee95@hotmail.com>
2020-01-19 22:09:47 +0900

    cofig setting IP 주소 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -42,7 +42,9 @@ SECRET_KEY = 'u=3o=!&g397g6c2qm%bvpujufi**32-7&m(f5w0i#g33g2ek1@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 AUTH_USER_MODEL = 'member.User'
 
